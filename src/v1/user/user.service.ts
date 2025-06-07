353dd25a1adcbcdc666a17ffc3aec9f8a70d70c7
Vuuou!wons <vinconloren@gmail.com>
2025-06-07 23:34:36 +0700

    tidied up user code

--- src/v1/user/user.service.ts
+++ src/v1/user/user.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { RegisterDto } from './dto/register.dto';
-import { LoginDto } from './dto/login.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+
 import { hashSync, compareSync } from "bcryptjs";
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
 import { jwtGenerate, jwtGenerateI } from '../../common/jwt';
+import { RegisterDto, LoginDto } from './dto';
 import { parseResponse } from '../dto/response';
 
 @Injectable()
