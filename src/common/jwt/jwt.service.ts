594f77071f050d8e81c0990ef68d6e3cfbe20a6b
Vuuou!wons <vinconloren@gmail.com>
2025-06-07 14:25:24 +0700

    properly imported jwt

--- src/common/jwt/jwt.service.ts
+++ src/common/jwt/jwt.service.ts
@@ -1,15 +1,16 @@
 import { jwtDecodedI, jwtGenerateI, jwtValidateI } from './jwt.interface';
-import { sign, verify } from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
+
 
 const jwtGenerate = (data: jwtGenerateI): string => {
-    const token = sign(data, process.env.JWT_SECRET);
+    const token = jwt.sign(data, process.env.JWT_SECRET);
     return token;
 }
 
 const jwtValidate = (token: string): jwtValidateI => {
     const payload: jwtValidateI = { status: false };
 
-    verify(token, process.env.JWT_SECRET, { maxAge: process.env.JWT_AGE }, (err: any, decoded: jwtDecodedI) => {
+    jwt.verify(token, process.env.JWT_SECRET, { maxAge: process.env.JWT_AGE }, (err: any, decoded: jwtDecodedI) => {
         if (err) {
             return payload;
         };
