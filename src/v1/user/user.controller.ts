9c0ac64f7fe1b05085a4c9725ac5f878619c4c75
Vuuou!wons <vinconloren@gmail.com>
2025-06-07 23:45:22 +0700

    added httpcode response

--- src/v1/user/user.controller.ts
+++ src/v1/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto';
@@ -7,11 +7,13 @@ import { LoginDto } from './dto';
 export class UserController {
   constructor(private readonly UserService: UserService) { }
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('register')
   register(@Body() registerBody: RegisterDto) {
     return this.UserService.register(registerBody);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() loginBody: LoginDto) {
     return this.UserService.login(loginBody);
