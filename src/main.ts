e51805ab8db9300ed2ff50cbfd693315d26a61d0
Vuuou!wons <vinconloren@gmail.com>
2025-06-07 13:48:44 +0700

    added global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConsoleLogger } from '@nestjs/common';
+import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +9,9 @@ async function bootstrap() {
       json: false
     })
   });
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/v1/user/dto/register.dto.ts
+++ src/v1/user/dto/register.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, IsAlphanumeric, IsNotEmpty, Length } from "class-validator";
+
 export class RegisterDto {
+    @IsAlphanumeric()
+    @IsNotEmpty()
+    @Length(8, 24)
     username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(8, 24)
     password: string;
 }
