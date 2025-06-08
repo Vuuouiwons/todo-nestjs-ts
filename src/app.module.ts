5bfada88f5ff652d4a33975b2e0454f73231ec77
Vuuou!wons <vinconloren@gmail.com>
2025-06-08 19:29:33 +0700

    added docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,11 @@ import { Todolist } from './v1/todolists/entities/todolist.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.PG_HOST,
-      port: Number(process.env.PG_PORT),
-      username: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Todo, Todolist],
       synchronize: true,
     }),
