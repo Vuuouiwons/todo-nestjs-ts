8f084ba069cec73233dec476e44a0fd0ef5cea9d
Vinsensius Reinard <vinconloren@gmail.com>
2026-01-05 01:14:11 +0700

    implemented basic token guard

--- src/guard/auth/auth.guard.ts
+++ src/guard/auth/auth.guard.ts
@@ -1,11 +1,23 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { UnauthorizedException } from '@nestjs/common';
+import { SecurityService } from 'src/lib/security/security.service';
+import { unauthorizedMessage } from 'src/common/constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(private securityService: SecurityService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const [authType, token] = request.headers.authorization?.split(' ');
+
+    if (authType !== 'Bearer') throw new UnauthorizedException(unauthorizedMessage);
+    if (!token) throw new UnauthorizedException(unauthorizedMessage);
+
+    const payload = await this.securityService.verifyToken(token);
+    request['user'] = payload;
+    
     return true;
   }
 }
